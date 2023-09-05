c059b2d2636955f6e00a79d89ef546d6f1daa825
Diego Medina <diegocab456@gmail.com>
2023-09-04 22:36:58 -0300

    implementacion basica del formulario reactivo

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   templateUrl: './basic-page.component.html',
@@ -7,4 +8,23 @@ import { Component } from '@angular/core';
 })
 export class BasicPageComponent {
 
+  /* params formcontrol(default_value, sync_validator, async_validator) -> FORMGROUP */
+  /*public myForm: FormGroup = new FormGroup({
+    name:         new FormControl(''),
+    price:        new FormControl(0),
+    inStorage:    new FormControl(0)
+  });*/
+
+  /* formcontrol(default_value, sync_validator, async_validator) -> FORMBUILDER */
+  public myForm: FormGroup = this.fb.group({
+    name:       [''],
+    price:      [0],
+    inStorage:  [0]
+  })
+
+  constructor( private fb: FormBuilder ) {}
+
+  public onSave(): void {
+    console.log(this.myForm.value);
+  }
 }
