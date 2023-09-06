95a1486641cab72939b081727dc3422a58c2dd26
Diego Medina <diegocab456@gmail.com>
2023-09-06 00:04:17 -0300

    implementacion mensajes de validacion al usuario - forms básicos

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -24,10 +24,35 @@ export class BasicPageComponent {
   constructor( private fb: FormBuilder ) {}
 
   public onSave(): void {
-    if (this.myForm.invalid)
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
       return;
+    }
 
     console.log(this.myForm.value);
     this.myForm.reset();
   }
+
+  public isValidField( field: string ): boolean | null {
+    return this.myForm.controls[field].errors && this.myForm.controls[field].touched
+  }
+
+  public getFieldError( field: string ): string | null {
+    if ( !this.myForm.contains(field) )
+      return null;
+
+    const errors = this.myForm.controls[field].errors || {};
+
+    for (const error of Object.keys(errors)) {
+      switch(error) {
+        case 'required':
+          return 'Este campo es requerido';
+        case 'minlength':
+          return `Mínimo ${ errors["minlength"].requiredLength } caracteres`;
+      }
+    }
+
+    return null;
+  }
+
 }
