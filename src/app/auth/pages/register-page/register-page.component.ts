3c56ddf48280e3c79dec77707785adf7efc0599e
Diego Medina <diegocab456@gmail.com>
2023-09-10 17:35:53 -0300

    Funciones externas de validacion - form de registro

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,15 +1,17 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
+import * as customValidators from 'src/app/shared/validators/validators';
+
 @Component({
   templateUrl: './register-page.component.html',
 })
 export class RegisterPageComponent {
 
   public myForm: FormGroup = this.fb.nonNullable.group({
-    name:             [ '', Validators.required ],
-    email:            [ '', Validators.required ],
-    username:         [ '', Validators.required ],
+    name:             [ '', [Validators.required, Validators.pattern( customValidators.firstNameAndLastnamePattern )] ],
+    email:            [ '', [Validators.required, Validators.pattern( customValidators.emailPattern )] ],
+    username:         [ '', [Validators.required, customValidators.cantBeStrider] ],
     password:         [ '', [Validators.required, Validators.minLength(6)] ],
     confirmPassword:  [ '', [Validators.required, Validators.minLength(6)] ],
   });
