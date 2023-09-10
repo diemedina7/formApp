0de9098f5fac3454f091014be8631468027b3ecc
Diego Medina <diegocab456@gmail.com>
2023-09-09 23:16:00 -0300

    Preparacion del form - form de registro

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './register-page.component.html',
-  styles: [
-  ]
 })
 export class RegisterPageComponent {
 
+  public myForm: FormGroup = this.fb.nonNullable.group({
+    name:             [ '', Validators.required ],
+    email:            [ '', Validators.required ],
+    username:         [ '', Validators.required ],
+    password:         [ '', [Validators.required, Validators.minLength(6)] ],
+    confirmPassword:  [ '', [Validators.required, Validators.minLength(6)] ],
+  });
+
+  constructor( private fb: FormBuilder ) {}
+
+  public isValidField( field: string ) {
+    //TODO: obtener validacion desde un service
+  }
+
+  public onSubmit() {
+    this.myForm.markAllAsTouched();
+  }
 }
